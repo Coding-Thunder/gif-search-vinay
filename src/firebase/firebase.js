80849a80fab7b7b907dcdd3c693f6115eac55e62
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2022-04-03 19:15:42 +0530

    made changes

---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyC-Q0hw6rJTth5tKzfieBdvYZxTYlp4aBw",
+  authDomain: "gif-search-7b725.firebaseapp.com",
+  projectId: "gif-search-7b725",
+  storageBucket: "gif-search-7b725.appspot.com",
+  messagingSenderId: "220235661421",
+  appId: "1:220235661421:web:f98190931e25ce626eff64",
+};
+
+// Initialize Firebase
+export const app = initializeApp(config);
